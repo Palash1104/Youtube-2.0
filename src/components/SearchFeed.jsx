9880f982d67b672693fcf63b05487ec059fa212b
Palash Tralshawala <tralpalash@gmail.com>
2024-04-04 13:46:40 +0530

    Search Functionality Implemented

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { useParams } from "react-router-dom";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box sx={{ p: 2 }}>
+      <Typography
+        variant="h4"
+        sx={{ color: "white", fontWeight: "bold", mb: 2 }}
+      >
+        Search Results For:{" "}
+        <span style={{ color: "#FC1305" }}>{searchTerm}</span> Videos
+      </Typography>
+
+      <Videos videos={[videos]} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
